6fc1d341bd4315fbde5cc345b410353a860cf0c5
Dmitriy Knyazev <deflexor@gmail.com>
2022-05-27 10:18:06 +0300

    fixes, etc

--- src/components/Wizard.vue
+++ src/components/Wizard.vue
@@ -2,18 +2,26 @@
 import Step1 from './wizard/Step1.vue'
 import Step2 from './wizard/Step2.vue'
 import Step3 from './wizard/Step3.vue'
+import ErrorAge from './wizard/ErrorAge.vue'
 import {ref} from 'vue'
+import { formData } from '../stores/form'
 
 const stepComponents = [null, Step1, Step2, Step3]
 
 const currentStep = ref(1)
+const hasError = ref(false)
 
 function stepChanged(step : number) {
-  currentStep.value = step
+  if (currentStep.value === 2 && step === 3 && formData.age > 100) {
+    hasError.value = true
+  } else {
+    currentStep.value = step
+  }
 }
 
 </script>
 
 <template>
-  <component @step="stepChanged" :is="stepComponents[currentStep]"></component>
+  <ErrorAge @ok="hasError = false" v-if="hasError"></ErrorAge>
+  <component @step="stepChanged" :is="stepComponents[currentStep]" v-else></component>
 </template>
